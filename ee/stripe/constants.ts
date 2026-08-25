ad67ccaca8c8d24a44f109d109a41ad325986572
patrickscherrer-spec <patrick.scherrer@scannit.io>
2026-08-25 14:09:58 +0200

    add stripe constants

---
+++ ee/stripe/constants.ts
@@ -0,0 +1,15 @@
+export enum PlanEnum {
+  Free = "free",
+  Pro = "pro",
+  Business = "business",
+  DataRooms = "datarooms",
+  DataRoomsPlus = "datarooms_plus",
+}
+
+export const PLANS = [
+  PlanEnum.Free,
+  PlanEnum.Pro,
+  PlanEnum.Business,
+  PlanEnum.DataRooms,
+  PlanEnum.DataRoomsPlus,
+];
